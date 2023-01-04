aa933db2ced22866067b36a46b5919c4ee5ccf81
your github username <your github email>
2023-01-04 16:38:58 +0000

    bestbuy-restassured-project-serenity-pradipkakadiya-01-01-2023

---
+++ src/main/java/com/bestbuy/constants/EndPoints.java
@@ -0,0 +1,22 @@
+package com.bestbuy.constants;
+
+
+public class EndPoints {
+
+    /**
+     * This is Endpoints of student api
+     */
+
+    public static final String GET_SINGLE_STORE_BY_ID = "/{storeID}";
+    public static final String UPDATE_STORE_BY_ID = "/{storeID}";
+    public static final String DELETE_STORE_BY_ID = "/{storeID}";
+
+    public static final String GET_SINGLE_PRODUCTS_BY_ID = "/{productID}";
+    public static final String UPDATE_PRODUCT_BY_ID = "/{productID}";
+    public static final String DELETE_PRODUCT_BY_ID = "/{productID}";
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+
+}
